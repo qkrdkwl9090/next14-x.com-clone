3f23507ed37d650e0fdf78d6cd716e14b618ff6b
Tony <qkrdkwl9090@naver.com>
2023-12-18 18:46:22 +0900

    feat(modal): intercepting routes

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -1,6 +1,6 @@
 import LoginModal from "@/app/(beforeLogin)/_components/LoginModal";
 
-// URL로 바로 접근 시, Modal을 띄워준다.
+// 첫 페이지에서 이동 시, 인터셉트하여 로그인 Modal을 띄워준다.
 export default function Page() {
   return <LoginModal />;
 }
--- src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/signup/page.tsx
@@ -1,6 +1,6 @@
 import SignupModal from "@/app/(beforeLogin)/_components/SignupModal";
 
-// URL로 바로 접근 시, Modal을 띄워준다.
+// 첫 페이지에서 이동 시, 인터셉트하여 회원가입 Modal을 띄워준다.
 export default function Page() {
   return <SignupModal />;
 }
