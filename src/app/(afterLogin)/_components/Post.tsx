059f91c8c544e112440b1cbb5247c85c78423f27
Tony <qkrdkwl9090@naver.com>
2023-12-19 10:58:25 +0900

    feat(home): add main section(post, tab)

---
+++ src/app/(afterLogin)/_components/Post.tsx
@@ -0,0 +1,68 @@
+import style from "./post.module.css";
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+import ActionButtons from "@/app/(afterLogin)/_components/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_components/PostArticle";
+import { faker } from "@faker-js/faker";
+import PostImages from "@/app/(afterLogin)/_components/PostImages";
+
+dayjs.locale("ko");
+dayjs.extend(relativeTime);
+
+type Props = {
+  noImage?: boolean;
+};
+export default function Post({ noImage }: Props) {
+  const target = {
+    postId: 1,
+    User: {
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/logo.png",
+    },
+    content: "테스트입니다. 테스트, 테스트",
+    createdAt: new Date(),
+    Images: [] as any[],
+  };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
+
+  return (
+    <PostArticle post={target}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            <div className={style.postShade} />
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div>
+            <PostImages post={target} />
+          </div>
+          <ActionButtons />
+        </div>
+      </div>
+    </PostArticle>
+  );
+}
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div>test</div>;
+import style from "./home.module.css";
+import Tab from "@/app/(afterLogin)/home/_components/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_components/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_components/PostForm";
+import Post from "@/app/(afterLogin)/_components/Post";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
