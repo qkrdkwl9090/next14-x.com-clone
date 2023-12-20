1f60b53e21ae26e6e199f078426e7b3cf024a779
Tony <qkrdkwl9090@naver.com>
2023-12-20 12:00:58 +0900

    refactor: move page module css in beforeLogin

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "@/app/page.module.css";
+import styles from "./page.module.css";
 import Link from "next/link";
 import Logo from "../../../public/logo.png";
 import cx from "classnames";
